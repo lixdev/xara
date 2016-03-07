cbd47febcd349bcc1e90b5025ecd5f77c699e1d4
xpose <xpose@xposeserver.www.xpose.co.ke>
2016-03-07 07:01:35 +0300

    new changes commited on monday

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -23,9 +23,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'cytonn',
+			'database'  => 'xpose',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'mysql',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
